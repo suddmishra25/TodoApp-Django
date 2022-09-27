954e608359b66ab2a11ae8715db120a0bf487d70
sudhanshu <mishrasudhanshu2512@gmail.com>
2022-09-27 23:05:02 +0530

    added adding and deleting feature

--- todolist/todo/models.py
+++ todolist/todo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    title = models.CharField(max_length=1000)
+    title = models.CharField(max_length=900,default = False)
 
     def __str__(self):
         return self.title
